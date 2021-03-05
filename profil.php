558b61b2fc090a75640910c00975920decd92528
ColsonB <bcolson@la-providence.net>
2021-03-05 18:14:28 +0100

    Mise à jour

--- profil.php
+++ profil.php
@@ -17,6 +17,7 @@
 
 <!DOCTYPE html>
 <html lang="fr">
+    
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -27,81 +28,16 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
         <script type="text/javascript" src="src/js/menu.js"></script>
         <script type="text/javascript" src="src/js/profil.js"></script>
-
     </head>
     
     <body>
+        
         <?php
             include("menu.php");
         ?>
+
         <div class="back">
-        
-        <?php
-    
-        ?>
-
-            <div>
-            <label oninput="range()"> Attaque
-                    <span id="value"></span>
-                    <input type="range" id="range" min="0" max="10" list="tickmarks" value="0" >
-                        <datalist id="tickmarks">
-                            <option value="0"></option>
-                            <option value="1"></option>
-                            <option value="2"></option>
-                            <option value="3"></option>
-                            <option value="4"></option>
-                            <option value="5"></option>
-                            <option value="6"></option>
-                            <option value="7"></option>
-                            <option value="8"></option>
-                            <option value="9"></option>
-                            <option value="10"></option>
-                        </datalist>
-                    <span id="value2"></span>
-                    <span id="value3"></span>
-                </label>
-                
-                <label oninput="range2()"> Défense
-                    <span id="value4"></span>
-                    <input type="range" id="range2" min="0" max="10" list="tickmarks" value="0">
-                        <datalist id="tickmarks">
-                            <option value="0"></option>
-                            <option value="1"></option>
-                            <option value="2"></option>
-                            <option value="3"></option>
-                            <option value="4"></option>
-                            <option value="5"></option>
-                            <option value="6"></option>
-                            <option value="7"></option>
-                            <option value="8"></option>
-                            <option value="9"></option>
-                            <option value="10"></option>
-                        </datalist>
-                     <span id="value5"></span>
-                    <span id="value6"></span>
-                </label>
-
-                <label oninput="range3()"> Vitesse
-                    <span id="value7"></span>
-                    <input type="range" id="range3" min="0" max="10" list="tickmarks" value="0">
-                        <datalist id="tickmarks">
-                            <option value="0"></option>
-                            <option value="1"></option>
-                            <option value="2"></option>
-                            <option value="3"></option>
-                            <option value="4"></option>
-                            <option value="5"></option>
-                            <option value="6"></option>
-                            <option value="7"></option>
-                            <option value="8"></option>
-                            <option value="9"></option>
-                            <option value="10"></option>
-                        </datalist>
-                    <span id="value8"></span>
-                    <span id="value9"></span>
-                </label>  
-            </div>
-            
+          
             <table>
                 <tr>
                     <td>Pseudo</td>
@@ -115,7 +51,7 @@
 
                 $reponse = $BDD->query("SELECT * FROM utilisateur
                 WHERE idUser = '$id'");
-                //$RequetStatement=$BDD->query($req);
+
                 while($Tab = $reponse->fetch()){
                     ?>
                         <tr>
@@ -130,6 +66,47 @@
                 }
             ?> 
             </table>
+
+            <form method="post" name=photoprofil>
+            <input type="file" required>
+            <input type="submit">
+            </form>
+
+            <?php
+
+    if (isset($_POST['photoprofil'])) {
+
+        $valideType = array('.jpg', '.jpeg', '.gif', '.png');
+        
+        if ($_FILES['img'] == 0) {
+            echo "aucun dossier selectionné";
+            die;
+        }
+
+        $fileType = ".".strtolower(substr(strrchr($_FILES['img']["pseudo"], '.'), 1));
+
+        $_FILES['img']["pseudo"] = $_SESSION['idUser']."_".$_FILES['img']["pseudo"];
+        
+        if (!in_array($fileType, $valideType)) {
+            echo "le fichier sélectionné n'est pas une image";
+            die;
+        }
+        $tmpName = $_FILES['img']['tmp_pseudo'];
+        $Name = $_FILES['img']['pseudo'];
+        $fileName = "images/utilisateur/" . $Name;
+        $résultUplod = move_uploaded_file($tmpName, $fileName);
+        if ($résultUplod) {
+            echo "transfere terminer";
+        }
+        $update = $MaBase->query("UPDATE `utilisateur` SET `photoProfil`='".$_FILES['img']['pseudo']."' WHERE $id =".$_SESSION['idUser']." ");
+            if($update){
+                echo "votre image a bien été changé";
+            }else{  
+                echo 'une erreur est survenue';
+            }
+        }
+
+            ?>
 
         </div>      
     </body>
